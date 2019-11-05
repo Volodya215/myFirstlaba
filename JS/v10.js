efc5279ea730350c62c7e242d90722923ba34f6a
Volodya215 <55815852+Volodya215@users.noreply.github.com>
2019-11-05 17:39:39 +0200

    js new

--- JS/v10.js
+++ JS/v10.js
@@ -1,26 +1,36 @@
-function ttest (s) {
-	var sres = 'Прочитаний текст:' + ' \r\n' + s + '\r\n' +
-        'Текст в стовпчик:' + '\r\n'
-	var cur = ''
+function ttest(s) {
+	var sres =
+		'Прочитаний текст:' +
+		' \r\n' +
+		s +
+		'\r\n' +
+		'Текст в стовпчик:' +
+		'\r\n';
+	var cur = '';
 	for (var i = 0; i <= s.length - 1; i += 1) {
-		c = s.charAt(i)
-		cur += c + '\r\n'
+		c = s.charAt(i);
+		cur += c + '\r\n';
 	}
-	sres += cur
-	return sres
+	sres += cur;
+	return sres;
 }
 
-function numword (obj) {
-	var h = obj.data.value
-	var s = obj.textin.value
-	s = ' ' + s + ' '
-	h = ' ' + h + ' '
-	var m = h.length
-	var res = 0
-	var i = 0
+function numword(obj) {
+	var h = obj.data.value;
+	var s = obj.textin.value;
+	s = ' ' + s + ' ';
+	h = ' ' + h + ' ';
+	var m = h.length;
+	var res = 0;
+	var i = 0;
 	while (i <= s.length - 1) {
-		ch = s.substr(i, m)
-		if (ch == h) { res += 1; i = i + m - 1 } else { i++ }
+		ch = s.substr(i, m);
+		if (ch == h) {
+			res += 1;
+			i = i + m - 1;
+		} else {
+			i++;
+		}
 	}
-	obj.result.value = res
+	obj.result.value = res;
 }
